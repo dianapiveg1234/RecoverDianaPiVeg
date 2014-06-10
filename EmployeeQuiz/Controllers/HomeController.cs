78f6b3b5b12e02c82f28f436a865304216c3a832
Carlos47197 <carlos_alfonso_e@hotmail.com>
2014-06-09 23:23:04 -0500

    final

---
+++ EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(Employe emp)
+        {
+
+            //Creo el objeto del modelo de datos
+            Payrolldm nomina = new Payrolldm(
+                @"E:\Recover\EmployeeQuiz\Models\EmployeeDb.csv");
+   
+            //busco el empleado dado su id
+            
+            var empleado=nomina.GetEmployeById(emp.Id);
+
+            //para que solo entren los numeros de control registrados
+            double comprobacion = double.Parse(emp.Id);
+
+
+            
+            if (comprobacion <= 91130079 && comprobacion >=91130075)
+            {
+                return View("WorkerView", empleado);
+            }
+
+                //en caso contrario te renderea a otra vista
+            else { return View("otra"); }
+        }
+
+
+
+
+    }
+}
---
+++ EmployeeQuiz/Models/Employe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeQuiz.Models
+{
+    public class Employe
+    {
+        public string Id {get;set;}
+        public string Name{ get; set; }
+        public string FirstLastname { get; set; }
+        public string SecondLastname { get; set; }
+        public string Position { get; set; }
+        public string Wage { get; set; }
+        public string Sex { get; set; }
+        public string PhothoPath { get; set; }
+ 
+    }
+}
